479188376486e7b2955cb37f1e7fceebc67c5b51
owang-587 <baiwen@ualberta.ca>
2023-03-11 18:50:51 -0500

    create project plan

---
+++ ShellSource/shell_simulation/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
