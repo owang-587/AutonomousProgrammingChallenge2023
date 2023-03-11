5f43d1f43ffd6f0a63419fd5dbb34fc6632958b2
owang-587 <baiwen@ualberta.ca>
2023-03-11 18:50:51 -0500

    create project plan

---
+++ ShellSource/shell_simulation/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export ROSLISP_PACKAGE_DIRECTORIES='/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build/devel/share/common-lisp'
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build/devel/lib:$LD_LIBRARY_PATH"
+export PATH='/opt/ros/noetic/bin:/home/owen/.local/bin:/home/owen/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PWD='/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build'
+export ROS_PACKAGE_PATH="/home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation:$ROS_PACKAGE_PATH"
