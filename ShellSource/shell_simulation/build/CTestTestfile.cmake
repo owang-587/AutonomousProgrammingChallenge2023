10facc31e15e9ddc4ad3450df3992924d866aa24
owang-587 <baiwen@ualberta.ca>
2023-03-13 22:25:12 -0400

    updates
    remove unnecessary files

--- ShellSource/shell_simulation/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation
-# Build directory: /home/owen/Documents/AutonomousProgrammingChallenge2023/ShellSource/shell_simulation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
